02665e666ddcb35ab6dd5089736d1cef269a416f
carlos delgado <carlos16931@hotmail.com>
2019-06-24 15:54:29 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appName: "platzimusic",
+    apiKey: "7e6d7ddd12a5165793c1fa96b1b50324",
+    secret: "f8f181cb335582d0c8beacfef1e1fea6",
+    registeredTo: "carlos841"
+}
+
+// Application name	platzimusic
+// API key	7e6d7ddd12a5165793c1fa96b1b50324
+// Shared secret	f8f181cb335582d0c8beacfef1e1fea6
+// Registered to	carlos841
